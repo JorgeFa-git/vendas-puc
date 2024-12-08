c7d1ca4afb117a1bb0b879b6ce32a565e8c7bfe7
Jorge_Antonio_Fr <jorgeantoniof@outlook.com>
2024-12-08 12:30:26 -0300

    Added product domain and repository

--- src/main/java/com/example/vendas/product/model/Product.java
+++ src/main/java/com/example/vendas/product/model/Product.java
@@ -3,6 +3,8 @@ package com.example.vendas.product.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Entity
 @Table
 @Data
@@ -12,5 +14,11 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    private
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    private int quantity;
 }
--- src/main/java/com/example/vendas/product/repository/ProductRepository.java
+++ src/main/java/com/example/vendas/product/repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.example.vendas.product.repository;
 
-public class ProductRepository {
+import com.example.vendas.product.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
